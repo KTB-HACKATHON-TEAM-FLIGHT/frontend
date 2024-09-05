e5da40c4bc9f43d5b326a01ba412d8c7fb745074
hl3470 <hl3470@naver.com>
2024-09-05 18:16:34 +0900

    feature: ListItem 컴포넌트 구현

---
+++ frontend/src/components/ListItem.jsx
@@ -0,0 +1,15 @@
+export default function ListItem({text, isClicked, onClick}) {
+
+    return (
+        <li>
+            <div
+                className={`block px-4 py-2 text-white rounded-md ${
+                    isClicked ? 'bg-blue-600' : 'hover:bg-gray-800'
+                }`}
+                onClick={onClick}
+            >
+                {text}
+            </div>
+        </li>
+    );
+}
