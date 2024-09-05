78239c6a74968cad4e1c15e6090daad7bfc4100b
hl3470 <hl3470@naver.com>
2024-09-05 18:16:20 +0900

    feature: BotChat 컴포넌트 구현

---
+++ frontend/src/components/BotChat.jsx
@@ -0,0 +1,11 @@
+export default function BotChat({text}) {
+    return (
+        <div className="flex items-start">
+            <img src="https://via.placeholder.com/40" className="w-10 h-10 rounded-full mr-3"
+                 alt="User Avatar"/>
+            <div className="bg-white p-3 rounded-lg shadow-md max-w-xs">
+                <p className="text-gray-800">{text}</p>
+            </div>
+        </div>
+    );
+}
