c79739f7d18b5013fcf864ad7a033b7fedcd3888
hl3470 <hl3470@naver.com>
2024-09-06 13:35:34 +0900

    refactor: css 변경

--- frontend/src/components/MarpitPPT.jsx
+++ frontend/src/components/MarpitPPT.jsx
@@ -1,18 +1,46 @@
 const {Marpit} = require('@marp-team/marpit');
 
+const overrideCss = `
+section {
+  padding: 2%;
+  width: 100%;
+  aspect-ratio: 16 / 9;
+  margin-bottom: 2%;
+}
+section {
+    box-sizing: border-box;
+    scroll-snap-align: center center;
+
+    display: flex; /* Flexbox를 사용하여 내용 정렬 */
+    flex-direction: column; /* 세로 정렬 */
+    justify-content: center; /* 수직 방향으로 중앙 정렬 */
+    align-items: center; /* 수평 방향으로 중앙 정렬 */
+}
+
+section * {
+    display: flex; /* Flexbox를 사용하여 내용 정렬 */
+    flex-direction: column; /* 세로 정렬 */
+    justify-content: center; /* 수직 방향으로 중앙 정렬 */
+    align-items: center; /* 수평 방향으로 중앙 정렬 */
+    text-align: center;
+}
+
+  `;
+
 export default function MarpitPPT({text, thema}) {
     const marpit = new Marpit()
-    marpit.themeSet.default= marpit.themeSet.add(thema)
+    marpit.themeSet.default = marpit.themeSet.add(thema)
 
     const {html, css} = marpit.render(text)
     return (
         <div className="overflow-y-auto h-full">
             <style>{css}</style>
+            <style>{overrideCss}</style>
             <div
                 style={{
                     width: '100%',
                 }}
-                dangerouslySetInnerHTML={{ __html: html }}
+                dangerouslySetInnerHTML={{__html: html}}
             />
         </div>
     );
