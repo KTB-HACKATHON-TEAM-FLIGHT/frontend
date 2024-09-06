f3442744cb2d822aa7c88060f3a2d7ec290d4361
hl3470 <hl3470@naver.com>
2024-09-07 03:44:32 +0900

    refactor: 목록 위치 변경

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -112,21 +112,21 @@ function App() {
               <li>
                 <a
                     href="#"
-                    className="block px-4 py-2 text-white hover:bg-gray-800 rounded-md"
-                >
-                  새 페이지
-                </a>
-              </li>
-              <li>
-                <a
-                    href="#"
                     className="block px-4 py-2 text-white bg-blue-600 rounded-md"
                 >
                   페이지1
                 </a>
               </li>
+              <li>
+                <a
+                    href="#"
+                    className="block px-4 py-2 text-white hover:bg-gray-800 rounded-md"
+                >
+                  새 페이지
+                </a>
+              </li>
             </ul>
-            <hr className="my-4 border-gray-700" />
+            <hr className="my-4 border-gray-700"/>
           </div>
 
           <div
