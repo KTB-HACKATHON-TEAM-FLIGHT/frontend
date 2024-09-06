58f105aefacc0cfd0c4faf5d8e0c642273e98148
extinctmule <extinctmule@gmail.com>
2024-09-06 19:16:55 +0900

    Fix: 오타 수정

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -92,7 +92,7 @@ function App() {
                         <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
                             <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo"/>
                             <span
-                                className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">ChatPTT</span>
+                                className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">ChatPPT</span>
                         </a>
                     </div>
                 </nav>
